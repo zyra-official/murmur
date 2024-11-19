2f8c3b5c70ca73d007cac99ac348417f1dcfd30a
thekbbohara <kbzcraft@email.com>
2024-11-19 21:30:41 +0545

    xx

---
+++ src/app/play/page.tsx
@@ -0,0 +1,31 @@
+"use client";
+import Hls from "hls.js";
+// import { useLayoutEffect, useRef, useState } from "react";
+import { useLayoutEffect, useRef } from "react";
+export default function Play() {
+  const audioRef = useRef<HTMLMediaElement | null>(null);
+  // const [audioSrcUrl, setAudioSrc] = useState(
+  //   "https://raw.githubusercontent.com/livekb/rumrum/refs/heads/main/1/output.m3u8",
+  // );
+  useLayoutEffect(() => {
+    if (Hls.isSupported()) {
+      const hls = new Hls();
+      // hls.loadSource(audioSrcUrl);
+      hls.loadSource(
+        "https://s3.ap-south-1.amazonaws.com/murmur.m3u8/navelhowtogetrich/output.m3u8",
+      );
+      if (!audioRef.current) return;
+      hls.attachMedia(audioRef.current);
+      hls.on(Hls.Events.MANIFEST_PARSED, () => {
+        audioRef.current?.focus();
+        debugger;
+      });
+    }
+  });
+
+  return (
+    <section className="overflow-hidden">
+      <audio ref={audioRef} controls></audio>
+    </section>
+  );
+}
--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -3,7 +3,6 @@
 import * as React from "react";
 import { Slot } from "@radix-ui/react-slot";
 import { VariantProps, cva } from "class-variance-authority";
-import { PanelLeft } from "lucide-react";
 
 import { useIsMobile } from "@/hooks/use-mobile";
 import { cn } from "@/lib/utils";
